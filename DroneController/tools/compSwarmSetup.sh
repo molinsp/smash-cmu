c3a930bfec412ee200288e4de8ef71509bebc845
Anton Dukeman <anton.dukeman@gmail.com>
2013-07-29 16:30:25 -0400

    DroneController: * Tools for swarm setup

--- DroneController/tools/compSwarmSetup.sh
+++ DroneController/tools/compSwarmSetup.sh
@@ -4,6 +4,5 @@ then
     echo "usage: $0 <ADAPTOR> <SSID> <ADDRESS>"
     echo "Sets the wireless adaptor to ad hoc mode with SSID and address"
 else
-    killall udhcpd
-    ip link set $1 down; iwconfig $1 mode ad-hoc; iwconfig $1 essid $2; ifconfig $1 $3; ip link set $1 up
+    sudo ip link set $1 down; sudo iwconfig $1 mode ad-hoc; sudo iwconfig $1 essid $2; sudo ifconfig $1 $3; sudo ip link set $1 up
 fi
