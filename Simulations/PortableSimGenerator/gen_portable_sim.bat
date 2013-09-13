e12aa364acfe2ba93dc5c2d256a46320cd3dc42c
secheverria <secheverria@sei.cmu.edu>
2013-09-13 16:29:34 -0400

    Sim:  * Improvements to portable generation scripts.  * Start of button creation for Sim takeoff and land.

--- Simulations/PortableSimGenerator/gen_portable_sim.bat
+++ Simulations/PortableSimGenerator/gen_portable_sim.bat
@@ -5,10 +5,10 @@ md %PORTABLE_FOLDER%
 
 REM Copy simulated drones.
 md %PORTABLE_FOLDER%\Drones
-copy SimulationFramework\start_simulated_drones.vbs  %PORTABLE_FOLDER%\Drones\
+copy ..\SimulationFramework\start_simulated_drones.vbs  %PORTABLE_FOLDER%\Drones\
 md %PORTABLE_FOLDER%\Drones\bin
-copy SimulationFramework\bin\*.exe  %PORTABLE_FOLDER%\Drones\bin
-copy SimulationFramework\bin\*.dll  %PORTABLE_FOLDER%\Drones\bin
+copy ..\SimulationFramework\bin\*.exe  %PORTABLE_FOLDER%\Drones\bin
+copy ..\SimulationFramework\bin\*.dll  %PORTABLE_FOLDER%\Drones\bin
 
 REM Add DLLs to drone folder.
 copy %MADARA_ROOT%\lib\*.dll   %PORTABLE_FOLDER%\Drones\bin
@@ -16,15 +16,20 @@ copy %ACE_ROOT%\lib\*.dll   %PORTABLE_FOLDER%\Drones\bin
 
 REM Copy scene.
 md %PORTABLE_FOLDER%\Scene
-copy CoverageAndBridgeScenes\* %PORTABLE_FOLDER%\Scene\
+copy ..\CoverageAndBridgeScenes\* %PORTABLE_FOLDER%\Scene\
 
-REM Copy plugins.
+REM Copy plugins to subfolder.
 md %PORTABLE_FOLDER%\Scene\Plugins
-copy SimulationFramework\lib\*.dll %PORTABLE_FOLDER%\Scene\Plugins
+copy ..\SimulationFramework\lib\*.dll %PORTABLE_FOLDER%\Scene\Plugins
+copy %MADARA_ROOT%\lib\*.dll   %PORTABLE_FOLDER%\Scene\Plugins
+copy %ACE_ROOT%\lib\*.dll   %PORTABLE_FOLDER%\Scene\Plugins
 
 REM Copy Vrep
 md %PORTABLE_FOLDER%\V-REP
 xcopy /E /EXCLUDE:vrep_exclusions.txt %VREP_HOME%\* %PORTABLE_FOLDER%\V-REP
+
+REM Copy plugins to VRep.
+copy %PORTABLE_FOLDER%\Scene\Plugins\*.dll %PORTABLE_FOLDER%\V-REP
 
 REM Copy start script.
 copy start_sim.bat %PORTABLE_FOLDER%\
