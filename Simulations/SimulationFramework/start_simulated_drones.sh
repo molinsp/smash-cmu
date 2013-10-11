5a83c197ea7684fb56590b4049db2dfc2f58dcdb
secheverria <secheverria@sei.cmu.edu>
2013-10-11 17:28:12 -0400

    SimulationFramework:  * Updating start script for Linux drones. Probably not working properly yet due to more parameters.

--- Simulations/SimulationFramework/start_simulated_drones.sh
+++ Simulations/SimulationFramework/start_simulated_drones.sh
@@ -1,15 +1,14 @@
 #!/bin/sh
 SLEEP=0.1
-REPO_ROOT=$HOME/smash-cmu
 if [ $# = 0 ]
 then
-    echo "usage: $0 <NUM_DRONES> <SLEEP:0.1>"
+    echo "usage: $0 -i <NUM_DRONES> <SLEEP:0.1>"
     echo "Launches <NUM_DRONES> with <SLEEP> between launches"
 else
     for i in $(seq 1 1 $1)
     do
         id=`expr $i - 1`
-        xterm -hold -l -e $REPO_ROOT/DroneController/install/bin/madara_client -i $id &
+        xterm -hold -l -e ./bin/DroneControllerSimulator -i $id &
         sleep $SLEEP
     done
 fi
