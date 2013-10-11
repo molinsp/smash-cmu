f8962bf04c7ea6737ee4b394b3259db02410625b
secheverria <secheverria@sei.cmu.edu>
2013-10-11 17:29:40 -0400

    Simulations:  * More fixes to starting script in Linux.

--- Simulations/SimulationFramework/start_simulated_drones.sh
+++ Simulations/SimulationFramework/start_simulated_drones.sh
@@ -2,7 +2,7 @@
 SLEEP=0.1
 if [ $# = 0 ]
 then
-    echo "usage: $0 -i <NUM_DRONES> <SLEEP:0.1>"
+    echo "usage: $0 <NUM_DRONES> <SLEEP:0.1>"
     echo "Launches <NUM_DRONES> with <SLEEP> between launches"
 else
     for i in $(seq 1 1 $1)
