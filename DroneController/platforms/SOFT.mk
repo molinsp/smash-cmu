8f8e54814242316b3abcbadac1918bfef637164b
James Root <jamesroot@gmail.com>
2013-12-06 16:19:37 -0500

    Testing the soft build with mpc

--- DroneController/platforms/SOFT.mk
+++
@@ -1,9 +0,0 @@
-######################################################################
-# Usage of this software requires acceptance of the SMASH-CMU License,
-# which can be found at the following URL:
-#
-# https://code.google.com/p/smash-cmu/wiki/License
-######################################################################
-
-LOCAL_SOURCES += platforms/soft/platform_soft_stubs.cpp
-LOCAL_SOURCES += platforms/kb_setup.cpp
